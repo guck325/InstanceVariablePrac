b418425fe2d8db0857669fb303f95acc55518846
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-05-09 12:07:20 +0900

    list에 들어있는 인스턴스 삭제할 때 주의 점 (잘못된 코드), 문제 해결 방법

--- InstanceVariablePrac/Program.cs
+++ InstanceVariablePrac/Program.cs
@@ -28,9 +28,12 @@ namespace InstanceVariablePrac
             //Console.WriteLine(Product1.origin); 자바에서는 가능
 
             List<Student> list = new List<Student>(); //student가 들어간 리스트 생성?
-            list.Add(new Student() { name = "곽경희", grade = 3 });
+            list.Add(new Student() { name = "곽경희", grade = 1 });
             list.Add(new Student() { name = "권하은", grade = 2 });
-            list.Add(new Student() { name = "김선혜", grade = 1 });
+            list.Add(new Student() { name = "김선혜", grade = 3 });
+            list.Add(new Student() { name = "김하늘", grade = 4 });
+            list.Add(new Student() { name = "배서연", grade = 1 });
+            list.Add(new Student() { name = "백지민", grade = 2 });
 
             List<Student> list2 = new List<Student>()
             {
@@ -43,6 +46,43 @@ namespace InstanceVariablePrac
                 Console.WriteLine(item);
             }
             foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            //list안에 인스턴스 지우기(예외가 발생한 코드)
+            //foreach(var item in list)
+            //foreach를 사용할 때 list에 변동이 있으면 안된다. foreach는 해결방법이 없다
+            //{
+            //if (item.grade == 3)
+            //{
+            //list.Remove(item);
+            //}
+            //}
+
+            //list안에 인스턴스 지우기(잘못된 코드)
+            for (int i=0; i<list.Count; i++)
+                //for 사용 중 list에 변동이 있으면 안된다. for문은 해결방법이 있다 (뒤에서부터 삭제)
+            {
+                if (list[i].grade > 1)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //뒤에서부터 삭제하면 문제를 피할 수 있다.
+            for (int i = list.Count-1; i >=0; i--)
+            {
+                if (list[i].grade > 1)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
